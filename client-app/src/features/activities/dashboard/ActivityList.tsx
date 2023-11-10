ab88294328d47435343aafef1fe1a7cc995f5fc0
Shriman Vinayagamoorthy <shriman30@hotmail.com>
2023-11-10 16:38:36 -0500

    Integrated MobX + refactoring

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,24 +1,23 @@
 import { SyntheticEvent, useState } from "react";
-import { Activity } from "../../../app/models/activity";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-interface Props {
-  activities: Activity[];
-  selectActivity:(id:string) =>void;
-  deleteActivity:(id:string)=>void;
-  submitting:boolean
-}
-export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default observer(function ActivityList() {
+  const { activityStore } = useStore()
+  const { deleteActivity, activitiesByDate, loading, selectActivity } = activityStore
   const [target, setTarget] = useState('');
 
-  function handleActivityDelete(event:SyntheticEvent<HTMLButtonElement>, id:string){
+  function handleActivityDelete(event: SyntheticEvent<HTMLButtonElement>, id: string) {
     setTarget(event.currentTarget.name);
     deleteActivity(id)
   }
   return (
     <Segment>
       <Item.Group divided>
-        {activities.map((activity) => (
+        {activitiesByDate.map((activity) => (
           <Item key={activity.id}>
             <Item.Content>
               <Item.Header as="a">{activity.title}</Item.Header>
@@ -28,9 +27,9 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                 <div>{activity.city}, {activity.venue}</div>
               </Item.Description>
               <Item.Extra>
-                <Button onClick={()=>selectActivity(activity.id)} floated="right" content="View" color="blue"/>
-                <Button name={activity.id} loading={submitting && target === activity.id} onClick={(e)=>handleActivityDelete(e,activity.id)} floated="right" content="Delete" color="red"/>
-                <Label basic content={activity.category}/> 
+                <Button onClick={() =>selectActivity(activity.id)} floated="right" content="View" color="blue" />
+                <Button name={activity.id} loading={loading && target === activity.id} onClick={(e) => handleActivityDelete(e, activity.id)} floated="right" content="Delete" color="red" />
+                <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>
@@ -38,4 +37,4 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
       </Item.Group>
     </Segment>
   );
-}
+})
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,14 +1,13 @@
 import { Button, Card, Image } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
+import LoadingComponent from "../../../app/layout/LoadingComponents";
 
-interface Props {
-  activity: Activity;
-  cancelSelectedActivity:()=>void;
-  openForm:(id:string)=>void;
 
-}
+export default function ActivityDetails() {
+  const {activityStore} = useStore();
+  const {selectedActivity:activity, openForm, cancelSelectedActivity} = activityStore;
 
-export default function ActivityDetails({ activity, cancelSelectedActivity, openForm }: Props) {
+  if(!activity) return <LoadingComponent content={"Loading..."}/>;
   return (
     <Card fluid>
       <Image
